f652766f88a7b494b6599e10207211a0f3e6ce70
Luciano Bracht <lucianoeletronico@gmail.com>
2024-10-04 16:37:31 -0300

    BUG_FIX: complete the env vars usage process and use it on db password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module'; // We'll create this soon
-import { User } from './users/user.entity'; // User entity
+import { UsersModule } from './users/users.module'; 
+import { User } from './users/user.entity'; 
+import { ConfigModule } from '@nestjs/config'; 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }), 
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'postgre-crud-nest-angular-crud.h.aivencloud.com',
       port: 11186, //5432
       username: 'avnadmin',
-      password: 'password', 
+      password: process.env.AIVEN_PASSWORD, 
       database: 'defaultdb',
       ssl: {
         rejectUnauthorized: false, // Or configure your CA certificate
