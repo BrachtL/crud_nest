81cabf0c999f15b8b3c9b971b03e41f26c21dd51
Luciano Bracht <lucianoeletronico@gmail.com>
2024-10-05 00:23:39 -0300

    FEATURE: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
-dotenv.config(); 
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS for all origins
+  app.enableCors(); // This will allow requests from all origins
+
+  // Optionally, you can specify specific origins:
+  // app.enableCors({ origin: 'http://localhost:4200' }); 
+
   await app.listen(3000);
 }
+
 bootstrap();
